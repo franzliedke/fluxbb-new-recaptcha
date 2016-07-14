512698a7cf0eed76e6c6b9319c6b0e5af7a22007
Franz Liedke <franz@develophp.org>
2016-07-14 23:31:08 +0900

    Add another curl option
    Closes #4.

--- addons/recaptcha.php
+++ addons/recaptcha.php
@@ -79,6 +79,7 @@ class addon_recaptcha extends flux_addon
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         curl_close($ch);
 
